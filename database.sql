430e289b578f65042bd7145ae8b284e2931a89ff
devon <devon_mckenzie@icloud.com>
2026-02-07 01:09:47 -0500

    Initial website, supabase and login is working, need to make ui nicer and add the framework for the upcoming workout log and training plan customization features.

--- database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                     username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    last_login DATETIME
-    );
